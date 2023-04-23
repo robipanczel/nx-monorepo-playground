b45a4d6dffea71139857366ec2881f2434a3b735
Róbert Pánczél <robi.panczel@gmail.com>
2023-04-23 13:39:17 +0200

    Create angular project

---
+++ apps/reactive/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule],
+  selector: 'nx-monorepo-playground-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {}
