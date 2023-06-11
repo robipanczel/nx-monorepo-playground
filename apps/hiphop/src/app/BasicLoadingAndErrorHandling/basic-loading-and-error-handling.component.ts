916d115efb0b8f447702b736edf6b9f42375c374
Róbert Pánczél <robi.panczel@gmail.com>
2023-06-11 23:47:19 +0200

    Maybe this is how it should be done?!

--- apps/hiphop/src/app/BasicLoadingAndErrorHandling/basic-loading-and-error-handling.component.ts
+++ apps/hiphop/src/app/BasicLoadingAndErrorHandling/basic-loading-and-error-handling.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FakeApiService } from '../fake-api.service';
+import { catchError, ignoreElements, of } from 'rxjs';
 
 @Component({
   selector: 'nx-monorepo-playground-basic-loading-and-error-handling',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>basic-loading-and-error-handling works!</p>`,
+  template: `
+    <ng-container
+      *ngIf="{
+        module: module$ | async,
+        moduleError: moduleError$ | async
+      } as data"
+    >
+      <div *ngIf="data.moduleError as moduleError">
+        Something went wrong: {{ moduleError }}
+      </div>
+
+      <div *ngIf="data.module as module; else loading">
+        {{ module }}
+      </div>
+
+      <ng-template #loading>
+        <div>Content is loading</div>
+      </ng-template>
+    </ng-container>
+  `,
   styles: [],
 })
-export class BasicLoadingAndErrorHandlingComponent {}
+export class BasicLoadingAndErrorHandlingComponent {
+  module$ = this.fakeApiService.getModule('asd123');
+  moduleError$ = this.module$.pipe(
+    ignoreElements(),
+    catchError((error) => of(error))
+  );
+
+  constructor(public readonly fakeApiService: FakeApiService) {}
+}
--- apps/hiphop/src/app/fake-api.service.ts
+++ apps/hiphop/src/app/fake-api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { concatMap, delay, of, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FakeApiService {
-
-  constructor() { }
+  getModule(moduleId: string) {
+    return of('asd123', 'qwe123', 'yxc123', 'vbn123', 'fgh123').pipe(
+      concatMap((module) => of(module).pipe(delay(3000))),
+      tap((module) => {
+        if (module === 'vbn123') {
+          throw new Error('BIG OOFFF');
+        }
+      })
+    );
+  }
 }
